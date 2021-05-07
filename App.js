3634c1de5533c22e3a3b61d70625003198bb74b0
Manuel Duarte <carlosd99m@gmail.com>
2021-05-07 00:49:13 -0600

    creando la estructura

--- App.js
+++ App.js
@@ -3,8 +3,27 @@ import {StyleSheet, View, Text, SafeAreaView} from 'react-native';
 
 export default function App() {
   return (
-    <SafeAreaView>
-      <Text>Hola Manuel</Text>
-    </SafeAreaView>
+    <>
+      <SafeAreaView style={styles.safeArea}>
+        <Text>Formulario head</Text>
+      </SafeAreaView>
+
+      <View>
+        <Text>Resultado</Text>
+      </View>
+
+      <View>
+        <Text>Text Footer</Text>
+      </View>
+    </>
   );
 }
+
+const styles = StyleSheet.create({
+  safeArea: {
+    backgroundColor: '#f00',
+    height: 200,
+    borderBottomLeftRadius: 30, // Para ponerlo redondo en la parte izquierda
+    borderBottomRightRadius: 30,
+  },
+});
