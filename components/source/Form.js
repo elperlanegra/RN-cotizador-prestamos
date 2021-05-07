20078abb22937af9c4c67633f75d8a4840cbc168
Manuel Duarte <carlosd99m@gmail.com>
2021-05-07 02:40:05 -0600

    Formulario de cotización :art:

--- components/source/Form.js
+++ components/source/Form.js
@@ -1,11 +1,52 @@
 import React from 'react';
-import {StylesSheet, TextInput, View} from 'react-native';
-import color from '../utils/colors';
+import {StyleSheet, TextInput, View} from 'react-native';
+import colors from '../utils/colors';
 
 export default function Form() {
   return (
-    <View>
-      <TextInput placeholder="Cantidad a pedir" />
+    <View style={styles.viewForm}>
+      <View style={styles.viewFormInputs}>
+        <TextInput
+          placeholder="Cantidad a pedir"
+          keyboardType="numeric"
+          style={styles.input}
+        />
+
+        <TextInput
+          placeholder="Interes %"
+          keyboardType="numeric"
+          style={styles.input}
+        />
+      </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  viewForm: {
+    position: 'absolute',
+    bottom: -90,
+    width: '85%',
+    paddingVertical: 50,
+    backgroundColor: colors.PRIMARY_COLOR_DARK,
+    borderRadius: 30,
+    height: 180,
+    justifyContent: 'center',
+  },
+  viewFormInputs: {
+    flexDirection: 'row',
+  },
+  input: {
+    height: 50,
+    backgroundColor: '#fff',
+    borderRadius: 1,
+    borderColor: colors.PRIMARY_COLOR,
+    borderRadius: 5,
+    width: '60%',
+    marginRight: 5,
+    marginLeft: -5,
+    marginBottom: 10,
+    color: '#000',
+    paddingHorizontal: 20,
+  },
+});
